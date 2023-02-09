6409223f37025ae2cf3ac28a30f39d7072bd1686
Łukasz Witkowski <contact@lukaszwitkowski.com>
2023-02-09 13:06:14 +0100

    Added hardcoded order and receipt printed to std output in App::main

--- src/main/java/com/lwitkowski/cafe/App.java
+++ src/main/java/com/lwitkowski/cafe/App.java
@@ -1,7 +1,16 @@
 package com.lwitkowski.cafe;
 
+import com.lwitkowski.cafe.domain.Menu;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! args=" + String.join(",", args));
+        var order = Menu.order(Menu.Coffee.small().twice())
+                .and(Menu.Coffee.large())
+                .and(Menu.Coffee.medium().withFoamedMilk())
+                .and(Menu.orangeJuice().times(3))
+                .and(Menu.baconRoll())
+                .thatWillBeAll();
+        
+        System.out.println(order.receipt());
     }
 }
